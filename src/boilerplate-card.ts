9e28ce4bf4d28821a682ebf4a3b443ade72bc389
Tor Inge Redalen <toringe@redalen.no>
2020-02-19 22:05:25 +0100

    init

---
+++ src/boilerplate-card.ts
@@ -0,0 +1,107 @@
+import { LitElement, html, customElement, property, CSSResult, TemplateResult, css, PropertyValues } from 'lit-element';
+import {
+  HomeAssistant,
+  hasConfigOrEntityChanged,
+  hasAction,
+  ActionHandlerEvent,
+  handleAction,
+  LovelaceCardEditor,
+  getLovelace,
+} from 'custom-card-helpers';
+
+import './editor';
+
+import { SensorButtonCardConfig } from './types';
+import { actionHandler } from './action-handler-directive';
+import { CARD_VERSION } from './const';
+
+import { localize } from './localize/localize';
+
+/* eslint no-console: 0 */
+console.info(
+  `%c  SENSOR-BUTTON-CARD \n%c  ${localize('common.version')} ${CARD_VERSION}    `,
+  'color: orange; font-weight: bold; background: black',
+  'color: white; font-weight: bold; background: dimgray',
+);
+
+// TODO Name your custom element
+@customElement('sensor-button-card')
+export class SensorButtonCard extends LitElement {
+  public static async getConfigElement(): Promise<LovelaceCardEditor> {
+    return document.createElement('sensor-button-card-editor') as LovelaceCardEditor;
+  }
+
+  public static getStubConfig(): object {
+    return {};
+  }
+
+  // TODO Add any properities that should cause your element to re-render here
+  @property() public hass?: HomeAssistant;
+  @property() private _config?: SensorButtonCardConfig;
+
+  public setConfig(config: SensorButtonCardConfig): void {
+    // TODO Check for required fields and that they are of the proper format
+    if (!config || config.show_error) {
+      throw new Error(localize('common.invalid_configuration'));
+    }
+
+    if (config.test_gui) {
+      getLovelace().setEditMode(true);
+    }
+
+    this._config = {
+      name: 'SensorButton',
+      ...config,
+    };
+  }
+
+  protected shouldUpdate(changedProps: PropertyValues): boolean {
+    return hasConfigOrEntityChanged(this, changedProps, false);
+  }
+
+  protected render(): TemplateResult | void {
+    if (!this._config || !this.hass) {
+      return html``;
+    }
+
+    // TODO Check for stateObj or other necessary things and render a warning if missing
+    if (this._config.show_warning) {
+      return html`
+        <ha-card>
+          <div class="warning">${localize('common.show_warning')}</div>
+        </ha-card>
+      `;
+    }
+
+    return html`
+      <ha-card
+        .header=${this._config.name}
+        @action=${this._handleAction}
+        .actionHandler=${actionHandler({
+          hasHold: hasAction(this._config.hold_action),
+          hasDoubleTap: hasAction(this._config.double_tap_action),
+          repeat: this._config.hold_action ? this._config.hold_action.repeat : undefined,
+        })}
+        tabindex="0"
+        aria-label=${`SensorButton: ${this._config.entity}`}
+      ></ha-card>
+    `;
+  }
+
+  private _handleAction(ev: ActionHandlerEvent): void {
+    if (this.hass && this._config && ev.detail.action) {
+      handleAction(this, this.hass, this._config, ev.detail.action);
+    }
+  }
+
+  static get styles(): CSSResult {
+    return css`
+      .warning {
+        display: block;
+        color: black;
+        background-color: #fce588;
+        padding: 8px;
+      }
+    `;
+  }
+}
